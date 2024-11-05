7ec3cc0754222d9a3d216aa5aeb732ed7a7dc1fa
Natanael <natanael.santos@aluno.ifsertao-pe.edu.br>
2024-11-04 21:07:17 -0300

    parametros

--- microblog/app.py
+++ microblog/app.py
@@ -5,6 +5,17 @@ def index():
     return render_template('index.html')
 @app.route('/contato')
 def contato():
-    return render_template('contato.html')
+    return render_template('contato.html', tel='(75)99814-2794', nome='tanzin')
+
+@app.route('/p1')
+def p1():
+    return render_template('p1.html')
+@app.route('/p2')
+def p2():
+    return render_template('p2.html', nc='natanael santos barbosa', id='22')
+# calculadora para somar dois números passados por parâmetros
+@app.route("/somar<int:num1>/int:num2")
+def soma (num1,num2):
+    return f'total'
 if __name__ == '__main__':
     app.run()
