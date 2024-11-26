0aa95968b2dfbf879ce2d8a547935197e7870f50
Natanael <natanael.santos@aluno.ifsertao-pe.edu.br>
2024-11-25 21:30:37 -0300

    nada

---
+++ pizzaria/app.py
@@ -0,0 +1,37 @@
+#python -m venv venv
+#.\venv\Scripts\activate
+#pip install flask
+# pip install python-dotenv
+#pip freeze > requirements.txt
+#pip install -r requirements.txt
+from flask import Flask, render_template, request
+
+app = Flask(_name_)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/cardapio')
+def cardapio():
+    pizzas = [
+        {
+            "nome": "Calabresa",
+            "ingredientes": "Molho de tomate, mussarela, calabresa, cebola e orégano",
+            "preco": 30.00
+        },
+        {
+            "nome": "Mussarela",
+            "ingredientes": "Molho de tomate, mussarela e orégano",
+            "preco": 25.00
+        },
+        {
+            "nome": "Portuguesa",
+            "ingredientes": "Molho de tomate, mussarela, presunto, ovos, cebola, azeitona e orégano",
+            "preco": 35.00
+        }
+    ]
+    return render_template("cardapio.html", pizzas=pizzas)
+
+if _name_ == '_main_':
+    app.run()
